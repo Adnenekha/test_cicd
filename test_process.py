e1dd2bc33ad8528c33874ee54e06e010dcf9776f
Adnenekha <adnene.khalbous@capgemini.com>
2024-07-04 15:32:42 +0200

    add new test

--- test_process.py
+++ test_process.py
@@ -3,5 +3,5 @@ from preprocess import Loader
 class TestPreprocess:
 
     def test_laoding(self):
-        assert Loader("Verrens-Arvey").filter_commune().shape != 0
+        assert Loader("Verrens-Arvey").filter_commune().shape[0] != 0
 
