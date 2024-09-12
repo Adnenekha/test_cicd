d6f684eeb0c7b847ced82565fe01e9afbc8e88fa
Adnenekha <adnene.khalbous@capgemini.com>
2024-09-12 16:41:50 +0200

    update test

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,10 +1,29 @@
-import sys
-sys.path.insert(0, '../')
-
 from src.preprocess import Loader
+import pytest
 
-class TestPreprocess:
+# class TestPreprocess:
 
-    def test_laoding(self):
-        assert Loader("Verrens-Arvey").filter_commune().shape[0] != 0
+#     def test_laoding(self):
+#         assert Loader("Verrens-Arvey").filter_commune().shape[0] != 0
 
+
+
+
+@pytest.mark.parametrize('value, key', [(2,3),(3,4)], )   
+def test_round_down_lesser_half(value, key):
+    result = value
+    assert result == key - 1
+
+
+
+@pytest.fixture
+def first_fixture():
+    data = {"first_name" : "Ranga",
+            "name" : "Gonnage"
+    }
+    return data
+
+def test_with_first_fxture(first_fixture):
+    print(first_fixture)
+    assert first_fixture["first_name"] == "Ranga"
+    assert first_fixture["name"] == "Gonnage"
